5b9f1abb30d6550d231695683ed13822c44c3747
shohbek <jalilovshohbek9909@gmail.com>
2023-11-15 11:31:29 +0500

    add react-pdf

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PDFViewer from "./PDFViewer";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PDFViewer />
+  </React.StrictMode>
+);
